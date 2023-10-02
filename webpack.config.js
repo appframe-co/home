18fd7910a8379c3c9c7dfbe894c1730b255c0871
DevHoliday <devholidaydev@gmail.com>
2023-10-03 02:03:47 +0300

    update

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const { NODE_ENV = 'production' } = process.env;
+
+module.exports = {
+  entry: './src/app.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    new CopyPlugin({
+        patterns: [
+          { from: "src/views", to: "views" },
+        ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  },
+  externals: [ nodeExternals() ]
+}
